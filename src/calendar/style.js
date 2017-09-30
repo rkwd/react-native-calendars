3b7cbf158b00d8d4188392595e426e23c0ece34b
rkwd <rkuzmanov@despark.com>
2017-09-30 19:20:24 +0300

    Remove calendar padding.

--- src/calendar/style.js
+++ src/calendar/style.js
@@ -5,8 +5,6 @@ export default function getStyle(theme={}) {
   const appStyle = {...defaultStyle, ...theme};
   return StyleSheet.create({
     container: {
-      paddingLeft: 5,
-      paddingRight: 5,
       flex: 1,
       backgroundColor: appStyle.calendarBackground
     },
